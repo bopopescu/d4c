17132a393be23cdf0c50e124a067c062a5083572
Neeme Takis <neeme@takis.ee>
2015-10-24 23:21:54 +0300

    got rid of global variables

--- basen/iomain_sauna.py
+++ basen/iomain_sauna.py
@@ -27,7 +27,7 @@ from droidcontroller.controller_app import *
 from droidcontroller.statekeeper import *
 
 from droidcontroller.read_gps import * #
-
+from droidcontroller.mybasen_send import * #
 
 from droidcontroller.panel_seneca import *
 panel = PanelSeneca(mb, mba = 3, mbi = 0, power = 0) # actual
@@ -43,26 +43,11 @@ requests_log = logging.getLogger("requests.packages.urllib3") # to see more
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-self_di = [0, 0, 0, 0]
-self_ledstates = [0, 0, 0, 0]
-self_fdvalue = 0
-self_panelpower = 0
+
+
 ts = time.time()
-channels2basen = {}
-channels2basen.update({0:['TempSauna','double',10]}) # name, type, divisor
-channels2basen.update({1:['TempBath','double',10]})
-channels2basen.update({2:['TempOutdoor','double',10]})
-channels2basen.update({3:['TempBattery','double',10]})
-channels2basen.update({4:['VoltageBatt','double',1000]})
-channels2basen.update({5:['FeetUp','double',None]})
-channels2basen.update({6:['DoorOpen','double',None]})
-channels2basen.update({7:['CoordLat','long',1]})
-channels2basen.update({8:['CoordLng','long',1]})
-channels2basen.update({9:['Light1','double',None]}) # with None do not round
-channels2basen.update({10:['Light2','double',None]}) # with None do not round
-channels2basen.update({11:['Light3','double',None]}) # with None do not round
-channels2basen.update({12:['Light4','double',None]}) # with None do not round
-values2basen = {} # id:value
+
+
 
 led = [] # lighting instances
 for i in range(4): # button triggers
@@ -77,6 +62,10 @@ class CustomerApp(object):
         self.path= 'tutorial/testing/sauna'
         self.url = 'https://mybasen.pilot.basen.com/_ua/'+self.aid+'/v0.1/data'
 
+        self.values2basen = {}
+        self.bs = MyBasenSend() # aid = 'itvilla', uid = b'itvilla', passwd = b'MxPZcbkjdFF5uEF9', path= 'tutorial/testing/test_async'
+        self.channels_setup()
+
         self.ca = ControllerApp(self.app)
         self.di = None
         self.footwarning = 0
@@ -85,6 +74,8 @@ class CustomerApp(object):
         self.panelpower = 0
         self.ledstates = [0, 0, 0, 0]
         self.fdvalue = 777
+        self.ts = time.time()
+
         self.charge_stop_timer = None
         self.gps = ReadGps(speed = 4800) # USB
         self.loop = tornado.ioloop.IOLoop.instance() # for gps and basen_send
@@ -93,14 +84,32 @@ class CustomerApp(object):
         print('ControllerApp instance created')
 
 
+    def channels_setup(self):
+        ''' channels for mybasen '''
+        channels2basen = {}
+        channels2basen.update({0:['TempSauna','double',10]}) # name, type, divisor
+        channels2basen.update({1:['TempBath','double',10]})
+        channels2basen.update({2:['TempOutdoor','double',10]})
+        channels2basen.update({3:['TempBattery','double',10]})
+        channels2basen.update({4:['VoltageBatt','double',1000]})
+        channels2basen.update({5:['FeetUp','double',None]})
+        channels2basen.update({6:['DoorOpen','double',None]})
+        channels2basen.update({7:['CoordLat','double',1]})
+        channels2basen.update({8:['CoordLng','double',1]})
+        channels2basen.update({9:['Light1','double',None]}) # with None do not round
+        channels2basen.update({10:['Light2','double',None]}) # with None do not round
+        channels2basen.update({12:['Light4','double',None]}) # with None do not round
+        channels2basen.update({11:['Light3','double',None]}) # with None do not round
+        self.bs.set_channels(channels2basen)
+
+
     def app(self, appinstance):
         ''' customer-specific things, like lighting control
         # switch on panelpower if any of led strips is on
         # switch off panelpower if all led strips are off
         '''
 
-        global ts, self_di, self_ledstates, self_fdvalue, self_panelpower, self_ts_gps, channels2basen, values2basen
-        ts = time.time()
+        self.ts = time.time()
         res= 0
         footwarning = 0
         shvalue = None
@@ -110,35 +119,35 @@ class CustomerApp(object):
 
         di = d.get_divalues('DIW')
         do = d.get_divalues('DOW')
-        if di != self_di:
+        if di != self.di:
             log.info('di changed: '+str(di)+', do: '+str(do)) ##
 
         try:
-            if self_di != None and di != self_di: # only changes
+            if self.di != None and di != self.di: # only changes
                 ledsum = 0
-                for i in range(4):
-                    if di[i] != self_di[i] and di[i] == 0: # change, press start
+                for i in range(len(di)):
+                    if di[i] != self.di[i] and di[i] == 0: # change, press start
                         led[i].toggle()
                     ledstate = led[i].get_state()
-                    if ledstate[0] != self_ledstates[i]:
+                    if ledstate[0] != self.ledstates[i]:
                         log.info('light '+str(i + 1)+' new state '+str(ledstate[0]))
-                        self_ledstates[i] = ledstate[0]
-                        values2basen.update({9+i : ledstate[0]}) # for basen
+                        self.ledstates[i] = ledstate[0]
+                        self.values2basen.update({9+i : ledstate[0]}) # for basen
                     d.set_dovalue('LTW', i+1, ledstate[0]) # actual output service, fixed in dchannels.py 13.9.2015
                     ledsum += ledstate[0] << i
                     if ledsum > 0:
-                        panelpower = 1
+                        self.panelpower = 1
                     else:
-                        panelpower = 0
+                        self.panelpower = 0
 
-                if panelpower != panel.get_power():
-                    log.info('NEW panelpower '+str(panelpower))
-                    panel.set_power(panelpower)
-                    d.set_dovalue('PPS',1,panelpower)
+                if self.panelpower != panel.get_power():
+                    log.info('NEW panelpower '+str(self.panelpower))
+                    panel.set_power(self.panelpower)
+                    d.set_dovalue('PPS',1,self.panelpower)
                 else:
-                    log.info('no change in panelpower '+str(panelpower))
+                    log.info('no change in panelpower '+str(self.panelpower))
 
-                self_di = di
+                self.di = di
 
             ## DATA FOR seneca S401 panel rows / via aochannels! panel update ##
             # temp temp temp temp aku jalg uks
@@ -195,26 +204,26 @@ class CustomerApp(object):
                             fdvalue = 0 # ok
                         #log.info('feet/door aivalue '+str(aivalue)+', shvalue '+str(fdvalue)) ##
 
-                        if fdvalue != 999 and fdvalue != self_fdvalue:
+                        if fdvalue != 999 and fdvalue != self.fdvalue:
                             d.set_divalue('FDW',1,(fdvalue & 1))
                             d.set_divalue('FDW',2,(fdvalue & 2) >> 1)
                             log.info('NEW feet/door aivalue '+str(aivalue)+', fdvalue '+str(fdvalue))
-                        self_fdvalue = fdvalue
+                        self.fdvalue = fdvalue
                         shvalue = (fdvalue & 1)
 
                 elif i == 6: # door
-                    shvalue = (self_fdvalue & 2) >> 1 # door bit in self_dvalue
+                    shvalue = (self.fdvalue & 2) >> 1 # door bit
 
                 #######
 
                 if i < 4:  # temperatures, i = 0..3
-                    values2basen.update({i : aivalue}) ## for mybasen portal
+                    self.values2basen.update({i : aivalue}) ## for mybasen portal
                     if aivalue != None:
                         shvalue = int(round(aivalue / 10.0, 0))
                     else:
                         shvalue = 9999 # sensor disconnected
                 else: # 4..6 here. 7,8 gpr, 9..12 lights
-                    values2basen.update({i : shvalue})
+                    self.values2basen.update({i : shvalue})
 
                 linereg = sorted(list(panel.get_data().keys()))[i]
                 panel.send(linereg, shvalue) ## sending to panel row with correct reg address
@@ -222,7 +231,7 @@ class CustomerApp(object):
                 ac.set_aivalue('PNW', i + 1, shvalue) # to report only
 
             d.sync_do() # actual output writing
-            self_di = di
+            self.di = di
             ##  end panel update ##
 
         except:
@@ -232,87 +241,21 @@ class CustomerApp(object):
 
     def gps_reader(self):
         ''' Read GPS '''
-        global values2basen
         try:
             coord = self.gps.get_coordinates()
             if coord != None and coord[0] != None and coord[1] != None:
                 ac.set_airaw('G1V',1,int(coord[0] * 1000000)) # lat
                 ac.set_airaw('G2V',1,int(coord[1] * 1000000)) # lng
-                values2basen.update({7 : coord[0]})
-                values2basen.update({8 : coord[1]})
+                self.self.values2basen.update({7 : coord[0]})
+                self.values2basen.update({8 : coord[1]})
             else:
                 log.warning('NO coordinates from GPS device, coord '+str(coord))
 
         except:
             print('gps ERROR')
-            traceback.print_exc()
-        self.basen_send() ## send once a minute to basen too
+            #traceback.print_exc()
 
-    def mybasen_rows(self):
-        ''' Create datta rows for mybasen '''
-        global values2basen, channels2basen
-        self.rows = []
-        for key in values2basen: # some channels may be without value in the beginning. add time?
-            # show chan, type, value
-            if channels2basen[key][2] != None:
-                value = values2basen[key] / channels2basen[key][2]
-            else:
-                value = int(values2basen[key])
-            #log.info('4basen '+str(channels2basen[key][0:2])+' '+str(value))
-            row = "{"
-            row += "\"channel\":\"" + str(channels2basen[key][0]) + "\","
-            row += "\"" + str(channels2basen[key][1]) + "\":" + str(value) # + ","
-            #row += "\"comment\":\"" + str(self.comment) + "\","
-            #row += "\"unit\":\"" + str(self.unit) + "\""
-            row += "}"
-            log.info(row)
-            self.rows.append(row)
-
-    def createhttpheaders(self):
-        '''create basic auth headers'''
-        authstr = 'aXR2aWxsYTpNeFBaY2JramRGRjV1RUY5' # in base64  FIXME
-            #"Basic %s" % (
-            #base64.b64encode("%s:%s" % (
-            #        self.uid, self.passwd)),)
-        self.httpheaders = {
-            "Content-Type": "application/json",
-            "Authorization": "Basic " + authstr
-            }
-        log.info('headers: '+str(self.httpheaders))
-
-
-    def domessage(self):
-        ''' Create json message for the given subpath and uid+password '''
-        # [{"dstore":{"path":"tutorial/testing/unit1","rows":[{"channels":[{"channel":"temp","double":23.3},{"channel":"weather","string":"Balmy"}]}]}}] # naide
-        # [{"dstore":{"path":"tutorial/testing/sauna","rows":[{"channels":[{"channel":"TempSauna","double":0.1},{"channel":"TempBath","double":0.2}]}]}}] # tekib ok
-        msg = '[{\"dstore\":{\"path\":'
-        msg += '\"' + self.path + '\",'
-        msg += '\"rows\":[{"channels":['
-        for row in self.rows:
-            msg += row
-            msg += ","
-        msg = msg.rstrip(",")
-        msg += ']}]}}]'  # close
-        log.debug('msg: '+str(msg))
-        return msg
-
-    def mybasen_send(self, message):
-        '''Send the message over https POST'''
-        self.createhttpheaders()
-        try:
-            #r = requests.post(self.url, data=message, headers=self.httpheaders) # NOT SUPPORTED!
-            r = requests.put(self.url, data=message, headers=self.httpheaders) # returns status code
-            log.info('response: '+str(r.content))
-        except:
-            logging.error("https connection to mybasen failed")
-            traceback.print_exc()
-            return False
-
-        #if r != 200:
-        #    logging.error("https connection response not ok "+str(r))
-        #    return False
-        return True
-
+        self.bs.basen_send(self.values2basen) ## send once a minute to basen too
 
 
     def charge_stop(self):
@@ -323,17 +266,11 @@ class CustomerApp(object):
             self.charge_stop_timer = None
 
 
-    def basen_send(self):
-        ''' the whole sending process with ioloop timer '''
-        global values2basen
-        if len(values2basen) > 0: # initially empty
-            self.mybasen_rows()
-            self.mybasen_send(self.domessage())
-
 
 
 ############################################
 cua = CustomerApp() # test like cua.ca.udp_sender() or cua.ca.app()
-# test: from iomain_sauna  import *; values2basen.update({1:2, 0:1}); cua.mybasen_rows(); mess=cua.domessage(); cua.mybasen_send(mess)
+# test: from iomain_sauna import *; cua.values2basen.update({1:2, 0:1}); cua.bs.basen_send(cua.values2basen)
+
 if __name__ == "__main__":
     tornado.ioloop.IOLoop.instance().start() # start your loop, event-based from now on
