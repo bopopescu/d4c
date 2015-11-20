2c87438c74ad4c0b18f5e7f0bcf2ab178a6c1f08
Neeme Takis <neeme@takis.ee>
2015-11-20 21:27:13 +0200

    pid values to debug svc, svc to kP...

--- k20kyte/iomain_k20kytevent.py
+++ k20kyte/iomain_k20kytevent.py
@@ -61,8 +61,8 @@ class CustomerApp(object):
         self.pwm_gas.append(IT5888pwm(mb, mbi = 0, mba = 1, name='floor_onTemp pwm control', period = 1000, bits = [14])) # do7, termostaadi kyte
         
         self.pid_gas = []
-        self.pid_gas.append(PID(P=1, I=0.1, D=0, min=5, max=995, name = 'gasheater watertemp pwm setpoint'))
-        self.pid_gas.append(PID(P=1, I=0.1, D=0, min=5, max=995, name = 'flooron watertemp pwm setpoint'))
+        self.pid_gas.append(PID(P=0.5, I=0.05, D=0, min=5, max=995, name = 'gasheater watertemp pwm setpoint'))
+        self.pid_gas.append(PID(P=0.5, I=0.05, D=0, min=5, max=995, name = 'flooron watertemp pwm setpoint'))
         
         self.TGW = [None, None, 380, None] # initial gas hot setpoint in [2] in ddegC
         self.THW = [None, None, 330, None] # initial floor_on setpoint in [2] in ddegC
@@ -113,18 +113,69 @@ class CustomerApp(object):
             # self.TGW, self.THW not used so far
             TGW = ac.get_aivalues('TGW') # water from gasheater - actual on, actual ret, setpoint, hilim
             THW = ac.get_aivalues('THW') # water to floors -  actual on, actual ret, setpoint, hilim
-            pwm_values = [ int(self.pid_gas[0].output(TGW[0],TGW[2])), int(self.pid_gas[1].output(THW[0],THW[2])) ]
+            KGPW = ac.get_aivalues('KGPW') # kP for loops G, H
+            KGIW = ac.get_aivalues('KGIW') # kI for loops G, H
+            KGDW = ac.get_aivalues('KGDW') # kD for loops G, H
+            
+            pwm_values = [ int(self.pid_gas[0].output(TGW[2],TGW[0])), int(self.pid_gas[1].output(THW[2],THW[0])) ]
             self.pwm_gas[0].set_value(13, pwm_values[0]) # pwm to heater knob, do bit 13
             self.pwm_gas[1].set_value(14, pwm_values[1]) # pwm to 3way valve, do bit 14
-            ac.set_aivalues('PWW', values = pwm_values)
             
-            if self.pid_gas[0].getMax() != TGW[3]:
+            tempvarsG = self.pid_gas[0].getvars() # dict
+            tempvarsH = self.pid_gas[1].getvars() # dict
+            
+            ac.set_aivalues('PWW', values = pwm_values)
+            ac.set_aivalues('LGGW', values = [int(10 * tempvarsG[outP]), int(1000 * tempvarsG[outI]), tempvarsG[outD] ]) # PID for loop 0
+            ac.set_aivalues('LGHW', values = [int(10 * tempvarsH[outP]), int(1000 * tempvarsH[outI]), tempvarsH[outD] ]) # PID for loop 1
+            ac.set_aivalues('NGIW', values = [tempvarsG[extnoint], tempvarsH[extnoint] ]) # ext int stop
+            
+            
+            ac.set_aivalues('KGPW', values = [int(10 * tempvarsG[Kp]), int(10 * tempvarsH[Kp]) ]) # kP for loops 0, 1
+            ac.set_aivalues('KGIW', values = [int(1000 * tempvarsG[Ki]), int(1000 * tempvarsH[Ki]) ]) # kI for loops 0, 1
+            ac.set_aivalues('KGDW', values = [int(tempvarsG[Kd]), int(tempvarsH[Kd]) ]) # kD for loops 0, 1
+            ''' 
+            {'Kp' : self.Kp, \
+            'Ki' : self.Ki, \
+            'Kd' : self.Kd, \
+            'outMin' : self.outMin, \
+            'outMax' : self.outMax, \
+            'outP' : self.Cp, \
+            'outI' : self.Ki * self.Ci, \
+            'outD' : self.Kd * self.Cd, \
+            'setpoint' : self.setPoint, \
+            'onlimit' : self.onLimit, \
+            'error' : self.error, \
+            'actual' : self.actual, \
+            'out' : self.out, \
+            'extnoint' : self.extnoint, \
+            'name': self.name }
+            '''
+            if tempvarsG[outMax] != TGW[3]:
                 self.pid_gas[0].setMax(TGW[3])
                 log.warning('pid_gas[0] hilim changed to '+str(TGW[3]))
-
-            if self.pid_gas[1].getMax() != THW[3]:
+            if tempvarsG[kP] != KGPW[0]:
+                self.pid_gas[0].setKp(KGPW[0] / 10.0)
+                log.warning('pid_gas[0] kP changed!')
+            if tempvarsG[kI] != KGIW[0]:
+                self.pid_gas[0].setKi(KGIW[0] / 1000.0)
+                log.warning('pid_gas[0] kI changed!')
+            if tempvarsG[kD] != KGDW[0]:
+                self.pid_gas[0].setKd(KGDW[0])
+                log.warning('pid_gas[0] kD changed!')
+                
+            if tempvarsH[outMax] != THW[3]:
                 self.pid_gas[1].setMax(THW[3])
                 log.warning('pid_gas[1] hilim changed to '+str(THW[3]))
+            if tempvarsH[kP] != KHPW[0]:
+                self.pid_gas[0].setKp(KHPW[0] / 10.0)
+                log.warning('pid_gas[0] kP changed!')
+            if tempvarsH[kI] != KHIW[0]:
+                self.pid_gas[0].setKi(KHIW[0] / 1000.0)
+                log.warning('pid_gas[0] kI changed!')
+            if tempvarsH[kD] != KHDW[0]:
+                self.pid_gas[0].setKd(KHDW[0])
+                log.warning('pid_gas[0] kD changed!')
+            
 
             log.info('gas_heater done, new pwm values '+str(pwm_values))
         except:
