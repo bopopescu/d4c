c71a9372b996c10f4141b473e091eec3868fd0b0
Neeme Takis <neeme@takis.ee>
2015-12-05 15:28:06 +0200

    valmis

--- mb1w.py
+++ mb1w.py
@@ -8,11 +8,11 @@ from droidcontroller.util_n import UN
 mba=int(sys.argv[1]) # mb aadress, no more arguments needed
 
 client = minimalmodbus.Instrument('/dev/ttyAPP0', mba)
-client.debug = True # valjastab saadetu ja saadu hex stringina, base64 abil
+client.debug = False # True # valjastab saadetu ja saadu hex stringina, base64 abil
 
 regcount = 36
 
 for regadd in [650, 750]: # ds18b20 ja ds2438
-    result = client.read_registers(regaddr,regcount)  # esimene on algusaadress, teine reg arv. fc 03
+    result = client.read_registers(regadd,regcount)  # esimene on algusaadress, teine reg arv. fc 03
     res = UN.onewire_hexid(result)
     print(regadd, res)
